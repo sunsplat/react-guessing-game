b253a31dcd162f31e94e907188c1ef8c06c7a33c
Ellen Sun <heeycutie@yahoo.com>
2018-02-10 21:34:34 -0500

    DEL unnecessary images

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-// import './semantic/dist/semantic.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
