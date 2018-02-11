007142f0342d27f426360fedd161a7fbb3492f18
Ellen Sun <heeycutie@yahoo.com>
2018-02-10 21:36:05 -0500

    Updates

--- src/components/guess-input.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function GuessInput(props) {
-    return (
-        <div className="ui input focus">
-            <input className={props.className} type={props.type} placeholder={props.placeholder} id={props.id} value={props.value} onChange={props.onChange} onClick={props.onClick}/>
-        </div>
-    );
-}
